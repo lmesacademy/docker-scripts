3d364fa8797fc9824e4309fa633a926f37e10685
Jacob Samro <JacobSamro@users.noreply.github.com>
2019-05-31 11:17:05 +0530

    Add Docker Compose Installer

--- install-docker/install.sh
+++ install-docker/install.sh
@@ -26,3 +26,7 @@ sudo apt-get update
 
 echo "Install latest version of Docker CE"
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "Installing Docker Compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
